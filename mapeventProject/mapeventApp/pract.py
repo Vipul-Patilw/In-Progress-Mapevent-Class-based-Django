647c5b5ffb24f53f2990af94e14dc0180972dc65
Vipul-Patilw <90356288+Vipul-Patilw@users.noreply.github.com>
2022-06-02 17:16:53 +0530

    CHANGES

---
+++ mapeventProject/mapeventApp/pract.py
@@ -0,0 +1,14 @@
+def pract1(list):
+    temp = list[0]
+    temp2 = list[len(list)-1]
+    list[len(list)-1]= temp
+    list[0] = temp2
+    a =list[0:len(list)]
+    
+    print(a)
+
+list = [1,2,3,4,5,6]
+pract1(list)
+   
+#select * from databasename.tablename where salary BETWWEN 50000 AND 60000;
+#select * from database.tablename where employe_name LIKE['A%']
