cf1799004a12809cfc33b7900bd746366c9f5326
Vipul-Patilw <90356288+Vipul-Patilw@users.noreply.github.com>
2022-06-06 10:05:29 +0530

    comit to main

--- mapeventProject/mapeventApp/admin.py
+++
@@ -1,9 +0,0 @@
-from django.contrib import admin
-from mapeventApp.models import  Event, Login,AddEvent
-from mapeventApp.models import Sign
-from mapeventApp.models import ChangePassword
-# Register your models here.
-admin.site.register(Login)
-admin.site.register(Event)
-admin.site.register(AddEvent)
-
